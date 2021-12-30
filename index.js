461f1ab65e85d87e86734da6399d4e738883d59c
Giovanni Reniero <giovanni.reniero@gmail.com>
2021-12-30 09:56:04 +0000

    functioning version to deploy

--- index.js
+++ index.js
@@ -3,24 +3,25 @@ const {hash}=window.location
 const message = atob(hash.replace("#", ""))
 const form = document.querySelector("form");
 const messageShow = document.querySelector("#message-show");
+const createMessage = document.querySelector("#createMessage");
 
 if(message){
-  form.classList.add(".hide");
+  createMessage.classList.add("hide");
   messageShow.classList.remove("hide")
-  document.querySelector("h1").innerHTML=message
+  document.querySelector("h2").innerHTML=message
 }
 
 form.addEventListener("submit", event =>{
   event.preventDefault(); //we have no back end server to receive the input-submit
   const input = document.querySelector("#message-input");
+  const inputbis = document.querySelector("#link-input");
+  const friendShare = document.querySelector("#friendShare");
+  // const createMessage = document.querySelector("#createMessage");
   const value = input.value;
   const encripted  = btoa(value);
-  const inputbis = document.querySelector("#link-input");
   inputbis.value = urlMaker(encripted);
-  const display = document.querySelector(".share");
-  const panel = document.querySelector(".card-panel");
-  display.classList.remove("hide");
-  panel.classList.add("hide");
+  friendShare.classList.remove("hide");
+  createMessage.classList.add("hide");
   inputbis.select();
 });
 
